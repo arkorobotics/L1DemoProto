3522aeac9f272662616530114eaba348d36eaea0
Arko <dotuplink@gmail.com>
2014-10-03 23:47:07 -0700

    Added boards
    Added boards

--- L1DemoProto.X/main.c
+++ L1DemoProto.X/main.c
@@ -33,59 +33,11 @@ _CONFIG2(FNOSC_FRCPLL & PLL96MHZ_ON & PLLDIV_DIV2)
 #define GFX_DISPLAY_BUFFER_START_ADDRESS        0x00000001ul
 #define GFX_DISPLAY_BUFFER_LENGTH               0x00012C00ul
 
-//__eds__ unsigned char GFXDisplayBuffer[GFX_DISPLAY_PIXEL_COUNT] __attribute__((eds));
-//__eds__ unsigned char GFXDisplayBufferBottom[GFX_DISPLAY_PIXEL_COUNT] __attribute__((eds));
-
-
 __eds__ unsigned char  __attribute__((far,section("eds1b"),space(eds) ,address(0x01F00))) GFXDisplayBuffer[ GFX_DISPLAY_PIXEL_COUNT ]; // Sample data buffer
-//__eds__ unsigned char  __attribute__((far,section("eds2b"),space(eds) ,address(0x09000))) GFXDisplayBufferBottom[ GFX_DISPLAY_PIXEL_COUNT ]; // Sample data buffer
 
-
-char message[] = "HACKADAY 10TH BIRTHDAY!\n";
+char message[] = "HAPPY 10TH BIRTHDAY HACKADAY! \n";
 
 void config_graphics(void) {
-    
-    /*
-    CLKDIVbits.G1CLKSEL = 1;    // Use 96Mhz Clk
-    CLKDIV2bits.GCLKDIV = 56;  // 16 for 30Hz, 6 for 60Hz
-
-    CLKDIVbits.PLLEN = 1;
-
-    G1CON2bits.DPMODE = 1;      // TFT
-    G1DBENbits.GDBEN = 0xFFFF;
-    G1DPWbits.DPW = 320;
-    G1DPHbits.DPH = 240;
-    G1DPWTbits.DPWT = 99 + 320;
-    G1DPHTbits.DPHT = 22 + 240;
-    G1CON3bits.DPCLKPOL = 1;    // Sample on falling edge
-    G1CON3bits.DPENOE = 1;
-    G1CON3bits.DPENPOL = 1;
-    
-    G1CON3bits.DPVSOE = 1;      
-    G1CON3bits.DPHSOE = 1;     
-    G1CON3bits.DPVSPOL = 0;
-    G1CON3bits.DPHSPOL = 0;
-    G1ACTDAbits.ACTLINE = 22;    // ver sum
-    G1ACTDAbits.ACTPIX = 88;    // hor sum
-    G1VSYNCbits.VSST = 4;   // front
-    G1HSYNCbits.HSST = 20;
-    G1VSYNCbits.VSLEN = 3;  //pulse
-    G1HSYNCbits.HSLEN = 30;
-    G1DBLCONbits.VENST = 4;
-    G1DBLCONbits.HENST = 20;
-
-    G1DPADRL = (unsigned long)(0x0000) & 0xFFFF;
-    G1DPADRH = 0;
-
-    G1CON2bits.DPBPP = 3;     // 8bpp mode
-    G1CON1bits.PUBPP = 3;
-
-    G1CON3bits.DPPINOE = 1;
-    
-    G1CON3bits.DPPWROE = 1;
-
-    G1CON2bits.DPTEST = 2;
-    */
     
     CLKDIVbits.RCDIV = 0;
 
@@ -152,7 +104,6 @@ void config_graphics(void) {
 
 
 int main(void) {
-    //OSCCON = 0x0000;
 
     CLKDIVbits.RCDIV = 0;      // 8 MHz
     CLKDIVbits.CPDIV = 0;
@@ -162,7 +113,6 @@ int main(void) {
     ANSD = 0x0000;
     ANSF = 0x0000;
     ANSG = 0x0000;
-    //TRISE = 0x0000;
 
     uint32_t i;
     for (i = 1; i < GFX_DISPLAY_PIXEL_COUNT; i++)
